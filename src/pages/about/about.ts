76859ff2d7ed9a5bcc536eadb4b1ed4274710d46
zainasaed <zainushka11@gmail.com>
2018-02-19 14:08:09 +0200

    zaina edit

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,21 +6,12 @@ import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-about',
-  templateUrl: 'home.html'
+  templateUrl: 'about.html'
 })
 export class AboutPage {
 
   constructor(public navCtrl: NavController,
     private afAuth: AngularFireAuth) { }
 
-  signInWithFacebook() {
-    this.afAuth.auth
-      .signInWithPopup(new firebase.auth.FacebookAuthProvider())
-      .then(res => console.log(res));
-  }
-
-  signOut() {
-    this.afAuth.auth.signOut();
-  }
 
 }
