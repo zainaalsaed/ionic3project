a645e6eed698879b556fd89b494f3651e1b0ec3a
zainasaed <zainushka11@gmail.com>
2018-02-19 10:38:55 +0200

    new

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 @Component({
   selector: 'page-about',
-  templateUrl: 'about.html'
+  templateUrl: 'home.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    private afAuth: AngularFireAuth) { }
 
+  signInWithFacebook() {
+    this.afAuth.auth
+      .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then(res => console.log(res));
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { AngularFireDatabase } from 'angularfire2/database';
-import { Observable } from 'rxjs/Observable';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
- 
-  constructor(public navCtrl: NavController, public db: AngularFireDatabase) {
-    
 
+  constructor(public navCtrl: NavController,
+    private afAuth: AngularFireAuth) { }
+
+  signInWithFacebook() {
+    this.afAuth.auth
+      .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then(res => console.log(res));
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
   }
 
 }
