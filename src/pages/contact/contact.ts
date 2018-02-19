c0b105d06845712e11a29e4cfaa122666edaa53e
zainasaed <zainushka11@gmail.com>
2018-02-19 14:01:59 +0200

    ok

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -9,41 +9,12 @@ import { FirebaseError } from '@firebase/util';
 })
 export class ContactPage {
 
-	@ViewChild('username') user;
-	@ViewChild('password') password;
 
   
   constructor(private alertCtrl: AlertController, private fire: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
   
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
-  }
 
-  alert(message: string) {
-    this.alertCtrl.create({
-      title: 'Info!',
-      subTitle: message,
-      buttons: ['OK']
-    }).present();
-  }
-
-
-  registerUser() {
-    this.fire.auth.createUserWithEmailAndPassword(this.user.value + '@domian.xta', this.password.value)
-    .then(data => {
-      console.log('got data ', data);
-     this.alert('Registered!');
-    })
-    
-  
-    
-    .catch((error:FirebaseError) => {
-      console.log('got an error ', error);
-     this.alert(error.message);
-    });
-  	console.log('Would register user with ', this.user.value, this.password.value);
-  }
 
 }
